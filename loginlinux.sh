10857686168004991bc08b6e8e12b22738097f41
junming.li <junming.li@tingwangdeMacBook-Pro.local>
2021-05-17 10:49:45 +0800

    add a file of loginlinux.sh

---
+++ loginlinux.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "正在登录Linux主机：192.168.66.137……"
+
+set timeout 300
+/usr/bin/expect -c "
+    spawn ssh root@192.168.66.137
+    expect {
+	\"*(yes/no)\" { send \"yes\r\"; exp_continue }
+	\"*password\" { send \"123123\r\"; exp_continue }
+    }
+expect eof"
